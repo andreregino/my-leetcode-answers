10d243277d6c6f4be2cb5673d20ff2402c6838e5
André Regino <andre_regino@hotmail.com>
2023-09-13 03:05:35 -0300

    Time: 42 ms (39.97%), Space: 16.3 MB (30.09%) - LeetHub

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,44 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        mapping = {
+            "1": "a",
+            '2': "b",
+            "3": "c",
+            "4": "d",
+            "5": "e",
+            "6": "f",
+            "7": "g",
+            "8": "h",
+            "9": "i",
+            "10": "j",
+            "11": "k",
+            "12": "l",
+            "13": "m",
+            "14": "n",
+            "15": "o",
+            "16": "p",
+            "17": "q",
+            "18": "r",
+            "19": "s",
+            "20": "t",
+            "21": "u",
+            "22": "v",
+            "23": "w", 
+            "24": "x",
+            "25": "y",
+            "26": "z"
+        }
+        final_string = ""
+        length = len(s)
+        for key, value in enumerate(s):
+            
+            if value == "#":
+                final_string += mapping[s[key-2] + s[key-1]]
+            elif key + 2 < length and s[key+2] == "#":
+                continue
+            elif key + 1 < length and s[key+1] == "#":
+                continue
+            else:
+                final_string += mapping[value]
+
+        return final_string
